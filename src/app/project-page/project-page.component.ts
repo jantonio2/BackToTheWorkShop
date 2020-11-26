228cc7c760616b4d2dfc648a998ecb7f0d160441
rodriguez-luis <pb.rndm@gmail.com>
2020-11-26 18:18:43 -0400

    Project Page

--- src/app/project-page/project-page.component.ts
+++ src/app/project-page/project-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DomSanitizer } from "@angular/platform-browser";
+import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-project-page',
   templateUrl: './project-page.component.html',
   styleUrls: ['./project-page.component.css']
 })
 export class ProjectPageComponent implements OnInit {
-
-  constructor() { }
+  item: any;
+  proyectos: any;
+  constructor(private sanitizer: DomSanitizer, private route: ActivatedRoute, private firestore:AngularFirestore) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.item = params.get('proId');
+    });
+    this.proyectos= this.firestore.collection('Proyecto').snapshotChanges();
   }
-
+  public getSantizeUrl(url : string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
